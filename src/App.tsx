c85d9b348e65f5eb7acd319a9bdea40c2d049eb3
GalinaMuratova <ufkz200147@gmail.com>
2023-05-25 17:10:31 +0600

    Добавила обработчик события удаления элемента. Завершила основное задание

--- src/App.tsx
+++ src/App.tsx
@@ -13,25 +13,31 @@ function App() {
     const [currentTask, setCurrentTask] = useState('');
 
     const inputChangeText = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setCurrentTask(event.target.value);
+            setCurrentTask(event.target.value);
     };
 
     const addTask = () => {
-        const taskId = String(Date.now());
+        const taskId = String(Math.random());
 
         const newTask = {
             id: taskId,
             text: currentTask
         };
+        console.log(tasks);
         setTask([...tasks, newTask]);
-        setCurrentTask('');
-    }
+    };
+    const deleteTask = (id: string) => {
+        const taskCopy = [...tasks];
+        const index = tasks.findIndex(task => task.id === id)
+        taskCopy.splice(index, 1);
+        setTask(taskCopy);
+    };
 
     return (
         <div className="App">
-            <AddTaskForm onInputChange={inputChangeText} onAddTask={addTask}/>
+            <AddTaskForm onInputChange={inputChangeText} onAddTask={addTask} />
             {tasks.map((task) => (
-                <Task id={task.id} text={task.text}/>
+                <Task key={task.id} text={task.text} remove={() => deleteTask(task.id)}/>
             ))}
         </div>
     );
--- src/Task/task.tsx
+++ src/Task/task.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 interface TaskProps {
-    id: string;
     text: string;
+    remove: React.MouseEventHandler;
 }
 const Task: React.FC<TaskProps> = props => {
     return (
         <div className="task">
           <span>{props.text}</span>
-          <button type="button">Delete</button>
+          <button type="button" onClick={props.remove}>Delete</button>
         </div>
     );
 };
