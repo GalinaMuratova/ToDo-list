35bf9d6c38d6d9f45e7d7f8fe4a348bb1bb84dce
GalinaMuratova <ufkz200147@gmail.com>
2023-05-25 14:34:00 +0600

    В основном компоненте App инициализировала state с несколькими начальными задачами.Вывела все задачи с помощью компонентов Task в цикле

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import AddTaskForm from "./AddTaskForm/addTaskForm";
 import Task from "./Task/task";
+import task from "./Task/task";
 
 function App() {
-  return (
-    <div className="App">
-        <AddTaskForm/>
-        <Task/>
-    </div>
-  );
+    const [tasks, setTask] = useState([
+        { id: '1', text: 'Text 1' },
+        { id: '2', text: 'Text 2' },
+        { id: '3', text: 'Text 3' },
+    ]);
+
+    return (
+        <div className="App">
+            <AddTaskForm/>
+            {tasks.map((task) => (
+                <Task key={task.id} text={task.text}/>
+            ))}
+        </div>
+    );
 }
 
 export default App;
+
--- src/Task/task.tsx
+++ src/Task/task.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const Task = () => {
+interface TaskProps {
+    key: string;
+    text: string;
+}
+const Task: React.FC<TaskProps> = props => {
     return (
         <div className="task">
-          <span>Task</span>
+          <span>{props.text}</span>
           <button type="button">Delete</button>
         </div>
     );
